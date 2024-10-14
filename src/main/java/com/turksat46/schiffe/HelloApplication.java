d84c30b39e23672ae347667c209de8be8f1715bb
Turksat46 <50042338+Turksat46@users.noreply.github.com>
2024-10-14 13:19:25 +0200

    Yeeee

--- src/main/java/com/turksat46/schiffe/HelloApplication.java
+++ src/main/java/com/turksat46/schiffe/HelloApplication.java
@@ -2,24 +2,33 @@ package com.turksat46.schiffe;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Stage stage = null;
+
     @Override
     public void start(Stage stage) throws IOException {
+        this.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 300, 420);
         stage.setTitle("Schiffe versenken");
         stage.setScene(scene);
         stage.show();
-
-        FXMLLoader yeloader = new FXMLLoader(HelloApplication.class.getResource("main-game-view.fxml"));
-        Scene yescene = new Scene(yeloader.load(), 900, 720);
-        stage.setScene(yescene);
     }
+
+    public void setScene(String pfadFXML) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(pfadFXML));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);      // Scene setzen
+        stage.show();               // Scene zeigen
+    }
+
+
 
     public static void main(String[] args) {
         launch();
