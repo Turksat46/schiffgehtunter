ffb4bcf2b867cf12b082cf505bee9c37c3cc1d1e
elion <elion.shahini74@gmail.com>
2024-10-25 15:10:35 +0200

    Spielfeld aufbau geändert, Spielfeld in array, und für jede zelle eine Action implementiert

--- src/main/java/com/turksat46/schiffgehtunter/Spielfeld.java
+++ src/main/java/com/turksat46/schiffgehtunter/Spielfeld.java
@@ -1,61 +1,64 @@
 package com.turksat46.schiffgehtunter;
 
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
 
 public class Spielfeld {
 
-    int belowoffset = 150;
-    int spielfeldgroesse;
-    int squaresize = 50;
-    int canvasgroesse;
-    Canvas canvas;
-    GraphicsContext gc;
+    int zellengroesse;
+    int[][] feld;
 
     public Spielfeld (int groesse){
-        this.spielfeldgroesse = groesse;
-        squaresize = getQuadratGroesse();
-        canvasgroesse = spielfeldgroesse*squaresize;
-        canvas = new Canvas(canvasgroesse, canvasgroesse+belowoffset);
-        gc = canvas.getGraphicsContext2D();
-        System.out.println ("Spielfeld groesse: " + canvasgroesse + " und groesse: "+groesse);
-        for (int i = 0; i < groesse; i++){
-            for (int j = 0; j < groesse; j++){
-                gc.setFill(Color.LIGHTBLUE);
-                gc.fillRect(j*squaresize, i*squaresize, squaresize-2, squaresize-2);
+        Stage stage = new Stage();
+
+
+        GridPane gridPane = new GridPane();
+
+
+        this.feld= new int [groesse][groesse];
+
+        // Schleife zur Erstellung der Zellen (als Rectangle mit Text)
+        for (int i = 0; i < groesse; i++) {
+            for (int j = 0; j < groesse; j++) {
+                int row = i;
+                int col = j;
+                this.feld[row][col] = 0;
+
+                // Rechteck und Text erstellen
+                Rectangle cell = new Rectangle(30, 30);
+                cell.setFill(Color.LIGHTBLUE);
+                cell.setStroke(Color.BLACK);
+
+                Text cellText = new Text(String.valueOf(feld[row][col]));
+
+                // StackPane für Rechteck und Text in einer Zelle
+                StackPane cellPane = new StackPane();
+                cellPane.getChildren().addAll(cell, cellText);
+
+                // Klick-Event für jede Zelle und aktualisiert einen neuen wert später also das löschen
+                cellPane.setOnMouseClicked(event -> {
+                    feld[row][col] = 1;
+                    cellText.setText("1");
+                });
+
+                // Zelle dem GridPane hinzufügen
+                gridPane.add(cellPane, j, i);
             }
         }
 
-        canvas.setOnMouseClicked(mouseEvent -> {
-            int col = (int)(mouseEvent.getSceneX()/squaresize);
-            int row = (int)(mouseEvent.getSceneY()/squaresize);
-            System.out.println(col+" "+row);
-        });
-
-        StackPane root = new StackPane(canvas);
-        Scene scene = new Scene(root, canvasgroesse, canvasgroesse+belowoffset);
-        Stage stage = new Stage();
+        //TODO: Hier wird aber neue fenster geöffnet veruschen in alte Fenster zu bleiben
+        Scene scene = new Scene(gridPane);
+        stage.setTitle("spielfeld");
         stage.setScene(scene);
+        stage.setWidth(320);
+        stage.setHeight(340);
         stage.show();
     }
 
-    private int getQuadratGroesse() {
-        int groesse = 0;
-        if(spielfeldgroesse <= 5){
-            return 75;
-        }
-        else if (spielfeldgroesse > 5 && spielfeldgroesse <= 10) {
-            return 50;
-        } else if (spielfeldgroesse > 10 && spielfeldgroesse <= 20) {
-            return 30;
-        }else{
-            return 20;
-        }
-    }
+
 }
