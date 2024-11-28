ee9444beafe017bd85ae6fa93f48e2fbf336455f
elion <elion.shahini74@gmail.com>
2024-11-28 17:04:44 +0100

    beide spielfelder in einem fenster

--- src/main/java/com/turksat46/schiffgehtunter/MainGameController.java
+++ src/main/java/com/turksat46/schiffgehtunter/MainGameController.java
@@ -3,10 +3,13 @@ import com.turksat46.schiffgehtunter.other.Ship;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.awt.*;
@@ -16,7 +19,9 @@ import java.util.ArrayList;
 public class MainGameController {
     @FXML
     public GridPane spielerstackpane, gegnerstackpane;
-    public static int currentState, currentMode, currentDifficulty, groesse;
+
+    @FXML public HBox container;
+   public static int currentState, currentMode, currentDifficulty, groesse;
     GridPane feld;
     AI bot;
 
@@ -30,7 +35,15 @@ public class MainGameController {
 
 
     public void setupSpiel(int groesse, Stage stage, int currentDifficulty, int currentMode){
-        spielerspielfeld = new Spielfeld(groesse, stage, spielerstackpane, false);
+        spielerspielfeld = new Spielfeld(groesse,  false);
+        gegnerspielfeld = new Spielfeld(groesse,  false);
+
+        spielerstackpane.getChildren().add(spielerspielfeld.gridPane);
+        gegnerstackpane.getChildren().add(gegnerspielfeld.gridPane);
+        // StackPane-Margen setzen
+        HBox.setMargin(spielerstackpane, new Insets(10, 10, 10, 10)); // Abstand für spielerstackpane
+        HBox.setMargin(gegnerstackpane, new Insets(10, 10, 10, 10)); // Abstand für gegnerstackpane
+
         this.currentMode = currentMode;
         this.groesse = groesse;
         currentState = 0;
@@ -40,12 +53,6 @@ public class MainGameController {
         bot = new AI(currentDifficulty, groesse);
 
         setPausierenEventHandler(stage);
-    }
-
-    public void setupGegnerFeld(int groesse, Stage stage) {
-        gegnerspielfeld = new Spielfeld(groesse, stage, gegnerstackpane, true);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        stage.setX((screenSize.width - stage.getWidth()) / 2 - stage.getWidth());
     }
 
     public void setPausierenEventHandler(Stage stage){
