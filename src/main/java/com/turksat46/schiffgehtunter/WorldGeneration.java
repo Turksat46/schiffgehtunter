bc1f5bea446dcad5172fec605f36baca59522002
elion <elion.shahini74@gmail.com>
2025-01-05 15:12:11 +0100

    in world generation ist bsp wie man des hovern machen kann

--- src/main/java/com/turksat46/schiffgehtunter/WorldGeneration.java
+++ src/main/java/com/turksat46/schiffgehtunter/WorldGeneration.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -19,69 +20,61 @@ import javafx.stage.Stage;
 
 public class WorldGeneration extends Application {
 
+
+    private double initialX;
+    private double initialY;
+
+
     @Override
     public void start(Stage stage) {
-        // GridPane erstellen
-        GridPane gridPane = new GridPane();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Button button = new Button("Button " + (i * 3 + j + 1));
-                gridPane.add(button, j, i);
-            }
-        }
+        BorderPane root = new BorderPane();
 
-        // VBox erstellen, die über dem GridPane "hovered"
-        HBox hoverBox = new HBox();
-        hoverBox.setStyle("-fx-background-color: rgba(255, 255, 0, 1);");
-        hoverBox.setMinSize(200, 100); // Größe der Box
-        hoverBox.setVisible(true); // Start als unsichtbar
+        // Create a draggable rectangle
+        Rectangle draggableRect = new Rectangle(64, 64, Color.BLUE);
+        makeDraggable(draggableRect);
 
-        final double[] initialMouseX = {0};
-        final double[] initialMouseY = {0};
+        // Create two target rectangles
+        Rectangle targetRect1 = createTargetRectangle();
+        Rectangle targetRect2 = createTargetRectangle();
 
-        // Setze die Position der hoverBox bei MousePressed
-        hoverBox.setOnMousePressed(event -> {
-            initialMouseX[0] = event.getSceneX() - hoverBox.getTranslateX();
-            initialMouseY[0] = event.getSceneY() - hoverBox.getTranslateY();
-        });
+        root.setCenter(targetRect1);
+        root.setRight(targetRect2);
 
-        // Bewege die hoverBox bei MouseDragged
-        hoverBox.setOnMouseDragged(event -> {
-            hoverBox.setTranslateX(event.getSceneX() - initialMouseX[0]);
-            hoverBox.setTranslateY(event.getSceneY() - initialMouseY[0]);
-        });
+        root.setLeft(draggableRect);
 
-        // Drag-Event für das GridPane
-        gridPane.setOnDragDetected(event -> {
-            Dragboard db = gridPane.startDragAndDrop(TransferMode.MOVE);
-            ClipboardContent content = new ClipboardContent();
-            content.putString("Some data");
-            db.setContent(content);
-            System.out.println("hallo");
-            event.consume();
-        });
-
-        gridPane.setOnDragEntered(event -> {
-            hoverBox.setLayoutX(event.getSceneX() - 100);
-            hoverBox.setLayoutY(event.getSceneY() - 50);
-            hoverBox.setVisible(true);
-
-            System.out.println("hallo");
-            event.consume();
-        });
-
-        gridPane.setOnDragExited(event -> {
-            hoverBox.setVisible(false);
-            event.consume();
-        });
-
-        // Root-Layout erstellen
-        Pane root = new Pane(); // Verwende Pane, um die Positionierung der Box zu ermöglichen
-        root.getChildren().addAll(gridPane, hoverBox);
-
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, 400, 150);
         stage.setScene(scene);
         stage.show();
+
+        // Continuously check for intersections
+        draggableRect.boundsInParentProperty().addListener((obs, oldBounds, newBounds) -> {
+            checkIntersection(draggableRect, targetRect1);
+            checkIntersection(draggableRect, targetRect2);
+        });
+    }
+
+    private void makeDraggable(Rectangle rect) {
+        rect.setOnMousePressed(event -> {
+            initialX = event.getSceneX() - rect.getTranslateX();
+            initialY = event.getSceneY() - rect.getTranslateY();
+        });
+
+        rect.setOnMouseDragged(event -> {
+            rect.setTranslateX(event.getSceneX() - initialX);
+            rect.setTranslateY(event.getSceneY() - initialY);
+        });
+    }
+
+    private Rectangle createTargetRectangle() {
+        return new Rectangle(100, 100, Color.LIGHTGRAY);
+    }
+
+    private void checkIntersection(Rectangle draggable, Rectangle target) {
+        if (draggable.getBoundsInParent().intersects(target.getBoundsInParent())) {
+            target.setFill(Color.GREEN); // Change to green on intersection
+        } else {
+            target.setFill(Color.LIGHTGRAY); // Reset to gray when not intersecting
+        }
     }
 
 
